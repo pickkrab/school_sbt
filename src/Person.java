d3ee58592b20e9a0f6d272b7d721fd8884ef7e7a
AlexanderLukoyanov <pickkrab@gmail.com>
2016-07-25 12:59:41 +0300

    #2

---
+++ src/Person.java
@@ -0,0 +1,37 @@
+/**
+ * Created by alexander on 25.07.16.
+ */
+public class Person {
+    private final boolean man;
+    private final String name;
+    private Person spouse;
+
+    public Person(boolean man, String name) {
+        this.man = man;
+        this.name = name;
+    }
+
+    public boolean marry(Person person) {
+        if (person.man != man) {
+            if (person.spouse != null) {
+                person.divorce();
+            }
+            if (spouse != null) {
+                spouse.divorce();
+            }
+            person.spouse = this;
+            this.spouse = person;
+            System.out.println("Свадьба " + person.spouse.name + " c " + this.spouse.name);
+        }
+        return person.man != man ? true : false;
+    }
+
+    public boolean divorce() {
+        if (this.spouse != null) {
+            System.out.println("Развод " + spouse.spouse.name + " c " + this.spouse.name);
+            spouse.spouse = null;
+            this.spouse = null;
+        }
+        return this.spouse != null ? true : false;
+    }
+}
--- src/Solution.java
+++ src/Solution.java
@@ -8,48 +8,14 @@ public class Solution {
         Person n3 = new Person(true, "Ruslan");
         Person n4 = new Person(false, "Diana");
         Person n5 = new Person(false, "Mangura");
-        n1.spouse = n2;
-        n2.spouse = n1;
-        n3.spouse = n5;
-        n4.spouse = null;
-        n5.spouse = n3;
 
+        n4.divorce();
         n3.marry(n2);
         n3.marry(n2);
         n4.marry(n1);
         n5.marry(n4);
-    }
-    public static class Person {
-        private final boolean man;
-        private final String name;
-        private Person spouse;
-
-        public Person(boolean man, String name) {
-            this.man = man;
-            this.name = name;
-        }
-
-        public boolean marry(Person person) {
-            if (person.man != man) {
-                if (person.spouse != null) {
-                    person.divorce();
-                }
-                if (spouse != null) {
-                    spouse.divorce();
-                }
-                person.spouse = this;
-                this.spouse = person;
-                System.out.println("Свадьба " + person.spouse.name + " c " + this.spouse.name);
-            }
-            return person.man != man ? true : false;
-        }
-
-        public boolean divorce() {
-            System.out.println("Развод " + spouse.spouse.name + " c " + this.spouse.name);
-            spouse.spouse = null;
-            this.spouse = null;
-
-            return true;
-        }
+        n1.divorce();
+        n4.divorce();
+        n4.marry(n3);
     }
 }
